88ae16f7b68ea86ce5b4d62b6e020f304b918298
eaturspinach <markmuskardin@gmail.com>
2011-08-31 23:22:12 -0700

    boilerplate authentication code - not that good

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,5 +1,21 @@
 class AuthenticationsController < ApplicationController
   def create
+    omniauth = request.env["omniauth.auth"]
+    authentication = Authentication.find_by_provider_and_uid(omniauth["provider"],omniauth["uid"])
+    if authentication
+      # flash[:notice] = "Signed in successfully"
+      redirect_to root_path
+    else
+      user = User.new
+      user.authentications.build(:provider => omniauth["provider"], :uid => omniauth["uid"])
+      user.name = omniauth["user_info"]["name"]
+      user.save!
+      redirect_to root_path
+    end
+    session[:name] = omniauth["user_info"]["name"]
+    session[:uid] = omniauth["uid"]
+    session[:provider] = omniauth["provider"]
+    session[:avatar] = omniauth["user_info"]["image"]
   end
 
   def index
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,13 @@ class SessionsController < ApplicationController
 
   def create
     #raise request.env['omniauth.auth'].to_yaml
-    flash[:notice] = "Successfully logged in"
-    session[:name] = request.env['omniauth.auth']['user_info']['name']
-    session[:uid] = request.env['omniauth.auth']['uid']
-    session[:provider] = request.env['omniauth.auth']['provider']
-    session[:avatar] = request.env['omniauth.auth']['user_info']['image']
-    session[:nickname] = request.env['omniauth.auth']['user_info']['nickname']
-    redirect_to root_path
+    # flash[:notice] = "Successfully logged in"
+    # session[:name] = request.env['omniauth.auth']['user_info']['name']
+    # session[:uid] = request.env['omniauth.auth']['uid']
+    # session[:provider] = request.env['omniauth.auth']['provider']
+    # session[:avatar] = request.env['omniauth.auth']['user_info']['image']
+    # session[:nickname] = request.env['omniauth.auth']['user_info']['nickname']
+    # redirect_to root_path
   end
 
   def failure
